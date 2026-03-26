abe8c5cad60d35d08cc82c8dc1106b8496a9278f
GM Mohit <gmmohit.designer@gmail.com>
2026-03-27 01:11:36 +0530

    auto-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedWorksSection from "@/components/FeaturedWorksSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
-import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <FeaturedWorksSection />
       <AboutSection />
       <ServicesSection />
-      <ContactSection />
       <Footer />
     </main>
   );
